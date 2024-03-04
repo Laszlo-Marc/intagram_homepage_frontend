0eefc804f2b1609f8942ac39ca6bd01d3548b816
914-Laszlo-Marc <marclaszlolucian@gmail.com>
2024-03-04 22:58:09 +0200

    intermediary version

--- src/timeline/Suggestions.js
+++ src/timeline/Suggestions.js
@@ -1,7 +1,38 @@
+import { Avatar } from "@mui/material";
 import React from "react";
 import "./Suggestions.css";
 function Suggestions() {
-  return <div>Suggestions</div>;
+  return (
+    <div className="suggestions">
+      <div className="suggestions__title">Sugestions for you</div>
+      <div className="suggestion__usernames">
+        <div className="suggestion__username">
+          <div className="username__left">
+            <span className="avatar">
+              <Avatar>U</Avatar>
+            </span>
+            <div className="username__info">
+              <span className="username">Username</span>
+              <span className="relation">New to instagram</span>
+            </div>
+            <button className="follow__button">Follow</button>
+          </div>
+        </div>
+        <div className="suggestion__username">
+          <div className="username__left">
+            <span className="avatar">
+              <Avatar>U</Avatar>
+            </span>
+            <div className="username__info">
+              <span className="username">Username</span>
+              <span className="relation">New to instagram</span>
+            </div>
+            <button className="follow__button">Follow</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Suggestions;
